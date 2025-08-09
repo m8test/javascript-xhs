204c235daed9139c1f56f591ce910fb89a962863
m8test <contact@m8test.com>
2025-08-09 08:29:31 +0800

    build: 更新 M8Test gradle版本到 0.1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 
 buildscript {
     dependencies {
-        val version = "0.1.7"
+        val version = "0.1.9"
         val jarPath = File(System.getProperty("user.home"), ".m8test/jar/gradle/${version}.jar")
         if (!jarPath.exists()) {
             try {
